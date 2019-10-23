8af3dbce8d5a69f9e650149bbf0e8036f1612aa9
aigik <aigi.kallaste@khk.ee>
2019-10-23 16:18:07 +0300

    andmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("JS test");
+// Andmetüübid
+
+// Täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// Reaalarvud - ujukomaarvud
+console.log(2.5);
+console.log(-7.23);
+console.log(-2.0);
+
+// sõned - tekst
+console.log('Hello world!')
+
+// Tõeväärtused
+console.log(true); // tõene
+console.log(false); // väär
+
+// nimekiri
+console.log([1, 2, 3, 4]);
+
+// objektid
+console.log({a : 1, b : 2});
+console.table ({a :1, b : 2});
