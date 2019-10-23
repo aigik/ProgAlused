fc47f69a86fe923fd6711777c20f80e0339b78d3
aigik <aigi.kallaste@khk.ee>
2019-10-23 16:24:45 +0300

    konsooli kasutamisvõimalused

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-// Andmetüübid
+// Logid konsoolile
 
-// Täisarvud
-console.log(10);
-console.log(-4);
-console.log(0);
+// logid
+console.log('123');
 
-// Reaalarvud - ujukomaarvud
-console.log(2.5);
-console.log(-7.23);
-console.log(-2.0);
+//tabel
+console.table({a : 1, b : 2});
 
-// sõned - tekst
-console.log('Hello world!')
+//veateade
+console.error('See on mingi viga');
 
-// Tõeväärtused
-console.log(true); // tõene
-console.log(false); // väär
+//märkused
+console.warn('See on märkus');
 
-// nimekiri
-console.log([1, 2, 3, 4]);
+// ajakulu kontroll
+console.time('Test');
+console.log('mingi tegevus');
+console.log('mingi tegevus');
+console.log('mingi tegevus');
+console.log('mingi tegevus');
+console.timeEnd('Test');
 
-// objektid
-console.log({a : 1, b : 2});
-console.table ({a :1, b : 2});
+// kustutamine
+console.clear();
