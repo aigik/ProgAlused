e3626ef54d873a8cf5b82d2fd7ca1604d17d2cb6
aigik <aigi.kallaste@khk.ee>
2019-10-28 10:55:50 +0200

    paaris ja paaritu number

--- app.js
+++ app.js
@@ -3,10 +3,17 @@
 // for tsükkel
 // üldtsükkel
 //let arv;
+let lause;
 for(let arv = 1; arv <= 10; arv++){
+    lause = 'arv = ';
     if(arv < 10){
-        console.log('arv = ' + arv);
+        lause = lause + ' ';
+    } 
+    lause = lause + arv;
+    if(arv % 2 == 0){
+        lause = lause + ' - paaris';
     } else {
-    console.log('arv = ' + arv);
+        lause = lause + ' - paaritu';
 }
+console.log(lause);
 }
