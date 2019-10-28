de65aa4e7bbd0c553585ca778cda8e5424b67744
aigik <aigi.kallaste@khk.ee>
2019-10-28 09:57:13 +0200

    tingimuslause I katse/ei tööta

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
-// Muutujad
+// Tingimuslause
 
-// var
-//muutujate defineerimine
-var nimi = 'Aigi'; // defineerimine
-console.log(nimi);
-nimi = 'Katrin'; // väärtuse omistamine
-console.log(nimi);
+// if
 
-// muutujate initsialiseerimine
-var tervitus;
-console.log(tervitus);
-tervitus = 'Tere tulemast!';
-console.log(tervitus);
+if (nimi == "Aigi"){
+    print = "Sinu nimi on Aigi";
+}else {
+    print = "Sinu nimi ei ole Aigi";
+}
 
-// liitnimede kasutamine
-var eesNimi = 'Aigi'; // Camel Style
-var ees_nimi = 'Aigi'; // PHP stiil
-var EesNimi = 'Aigi'; // Pascal Style
-var eesnimi = 'Aigi';
-
-// let
-let pereNimi = 'Kallaste';
-console.log(pereNimi);
-pereNimi = '???';
-console.log(pereNimi);
-
-// const
-const minuEesNimi = 'Aigi';
-console.log(minuEesNimi);
-// minuEesNimi = 'Katrin';
-
-// const kasutamise näide
-const inimene = {nimi : 'Katrin', vanus : 20};
-console.table(inimene);
-inimene.vanus = 21;
-console.table(inimene);
-
-// const kasutamine näide 2
-const arvud = [1, 2, 3, 4, 5];
-console.log(arvud);
-arvud.push(6);
-console.log(arvud);
+if (nimi = "Aigi"){
+    alert("Sinu nimi on Aigi");
+}
