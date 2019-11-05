c1f0561d09d1a40ddbbf73b487eaea57d9e36210
aigik <aigi.kallaste@khk.ee>
2019-11-05 15:07:43 +0200

    funktsioon ristküliku pindala return

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 // funktsioon, mis arvutab ristküliku pindala ja väljastab ekraanile
 
 function ristkylikuPindala(pikkus, laius){
+    // väljastame väärtused
     console.log('Pikkus = ' + pikkus);
     console.log('Laius = ' + laius);
     // arvutame pindala
     let pindala = pikkus * laius;
-    // väljastame pindala
-    console.log('Pindala = ' + pindala);
+    // tagastame pindala põhiprogrammi
+    return pindala;
+    
 }
 
-ristkylikuPindala(15, 5);
+rPindala = ristkylikuPindala(15, 5);
+console.log('Pindala = ' + rPindala);
+
