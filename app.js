95d872e5671f76c287a6f72769447bb9781831bc
aigik <aigi.kallaste@khk.ee>
2019-11-13 21:21:52 +0200

    muutuja scope - plokk

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// N2htavus - scope
-// globaalne
-var a = 1;
 let b = 2;
 const c = 3;
-console.log('Globaalne: ', a, b, c);
+
+// ploki p6hine
+if(true){
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Plokk: ', a, b, c);
+}
+
+for(var a = 0; a < 10; a++){
+    console.log('Tsykkel:', a);
+}
+ console.log('Globaalne: ', a, b, c);
