95e3fbf6656955a6764adf4169dad41f58588b55
aigik <aigi.kallaste@khk.ee>
2019-11-13 22:09:48 +0200

    Loodud OOP kursuse failide p6hi

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-let b = 2;
-const c = 3;
 
-// ploki p6hine
-if(true){
-    var a = 4;
-    let b = 5;
-    const c = 6;
-    console.log('Plokk: ', a, b, c);
-}
-
-for(var a = 0; a < 10; a++){
-    console.log('Tsykkel:', a);
-}
- console.log('Globaalne: ', a, b, c);
